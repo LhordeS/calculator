1cf74be95f677e55e08372179284f99653aef1e1
LhordeS <lhorde@msn.com>
2022-08-05 10:44:12 +0900

    Complete full functionality, but still without decimal input button

--- app.js
+++ app.js
@@ -24,30 +24,109 @@ const btn1 = document.querySelector(".btn-1");
 const display = document.querySelector(".display");
 const Numberbuttons = document.querySelectorAll(".number-btns");
 const operatorbuttons = document.querySelectorAll(".operator-btns");
+const clearButton = document.querySelector(".btn-clear");
+const equalButton = document.querySelector(".btn-equal");
 
 let btnPressNum1 = 0;
 let btnPressNum2 = 0;
 let btnPressStr = [];
 let btnPressOp;
+let answer = 0;
 
 Numberbuttons.forEach(function (button) {
   button.addEventListener("click", (e) => {
-    value = e.target.textContent;
-      btnPressNum = Number(btnPressNum + value);
-      console.log(btnPressNum);
-    // btnPressStr = btnPressStr + '1';
-    btnPressStr.push(value);
-    // btnPressStr.split(',')
-    display.textContent = btnPressStr.join("");
+    if (btnPressOp == undefined) {
+      value = e.target.textContent;
+      btnPressNum1 = Number(btnPressNum1 + value);
+      btnPressStr.push(value);
+      display.textContent = btnPressStr.join("");
+    } else if ((btnPressOp != undefined && btnPressNum2 === 0) || answer > 0) {
+      display.textContent = "";
+      btnPressNum2 = 0;
+      value = e.target.textContent;
+      btnPressStr = [];
+      btnPressNum2 = Number(btnPressNum2 + value);
+      btnPressStr.push(value);
+      display.textContent = btnPressStr.join("");
+    } else {
+      value = e.target.textContent;
+      btnPressNum2 = Number(btnPressNum2 + value);
+      btnPressStr.push(value);
+      display.textContent = btnPressStr.join("");
+    }
   });
 });
 
+
+
 operatorbuttons.forEach(function (button) {
-    button.addEventListener('click', (e) => {
-        if (e.target.classList.contains('btn-add')) btnPressOp = add;
-        if (e.target.classList.contains('btn-subtract')) btnPressOp = subtract;
-        if (e.target.classList.contains('btn-multiply')) btnPressOp = multiply;
-        if (e.target.classList.contains('btn-divide')) btnPressOp = divide;
-        console.log(btnPressOp);
-    })
-})
+  button.addEventListener("click", (e) => {
+    const containsAdd = e.target.classList.contains('btn-add');
+    const containsSubtract = e.target.classList.contains('btn-subtract');
+    const containsMultiply = e.target.classList.contains('btn-multiply');
+    const containsDivide = e.target.classList.contains('btn-divide');
+
+    function recurCalc() {
+      answer = operator(btnPressOp, btnPressNum1, btnPressNum2);
+      btnPressNum1 = answer;
+      display.textContent = answer;
+    }
+
+    //Additions
+    if (containsAdd && btnPressNum2 === 0) {
+      btnPressOp = add;   
+    } else if (containsAdd && btnPressNum2 > 0) {
+      recurCalc();
+      btnPressOp = add;
+    }
+
+    //Subtractions
+    if (containsSubtract && btnPressNum2 === 0) {
+      btnPressOp = subtract;
+    } else if (containsSubtract && btnPressNum2 > 0) {
+      recurCalc();
+      btnPressOp = subtract;
+    }
+
+
+    //Multiplications
+    if (containsMultiply && btnPressNum2 === 0) {
+      btnPressOp = multiply;
+    } else if (containsMultiply && btnPressNum2 > 0) {
+      recurCalc();
+      btnPressOp = multiply;
+    }
+
+
+    //Divisions
+    if (containsDivide && btnPressNum2 === 0) {
+      btnPressOp = divide;
+    } else if (containsDivide && btnPressNum2 > 0) {
+      recurCalc();
+      btnPressOp = divide;
+    }
+
+  });
+});
+
+clearButton.addEventListener("click", (e) => {
+  init();
+});
+
+equalButton.addEventListener("click", () => {
+  if (btnPressNum1 != 0 && btnPressNum2 != 0 && btnPressOp != undefined) {
+    answer = operator(btnPressOp, btnPressNum1, btnPressNum2);
+    display.textContent = answer;
+  }
+});
+
+function init() {
+  btnPressNum1 = 0;
+  btnPressNum2 = 0;
+  btnPressStr = [];
+  btnPressOp = undefined;
+  answer = 0;
+  display.textContent = "";
+}
+
+// console.log(answer);
